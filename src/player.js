425d49292ab02a0e0e0e12b2860cb2f1fe063a9f
Mughammad <caseameen44@gmail.com>
2026-02-21 00:36:25 +0200

    feat: implement player class with base stats

---
+++ src/player.js
@@ -0,0 +1,15 @@
+export class Player {
+  constructor() {
+    this.age = 25;
+    this.month = 1;
+    this.year = 2026;
+
+    this.money = 0;
+    this.health = 70;
+    this.discipline = 30;
+    this.happiness = 40;
+    this.reputation = 10;
+
+    this.alive = true;
+  }
+}
