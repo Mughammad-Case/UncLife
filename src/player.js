310577342004aef86734e3ced73830a21cdc413f
Mughammad <caseameen44@gmail.com>
2026-02-22 06:16:07 +0200

    feat stat clamping for stats to stay between 0 - 100 range

--- src/player.js
+++ src/player.js
@@ -20,16 +20,28 @@ export class Player {
       this.month = 1;
       this.year++;
       this.age++;
-    }
+    } // Age up the Unc and updates each year after 12 months
 
     this.health -= 1;
-    if (this.health <= 0) this.health = 0; // Ensure health doesn't go negative
-
     this.happiness -= 2;
-    if (this.happiness <= 0) this.happiness = 0;
-
     this.money += 100;
     this.discipline += 1;
-    this.reputation += 1;
-  } // Logic to age up the player each month and year, increasing age by 1 each year and adjusting stats accordingly.
-} // Player class with initial properties to import into the game.
+    this.reputation += 1; // Aging up the Unc also affects their stats each month
+
+    this.clampStats();
+  }
+
+  clampStats() {
+    if (this.health > 100) this.health = 100;
+    if (this.health < 0) this.health = 0;
+
+    if (this.happiness > 100) this.happiness = 100;
+    if (this.happiness < 0) this.happiness = 0;
+
+    if (this.discipline > 100) this.discipline = 100;
+    if (this.discipline < 0) this.discipline = 0;
+
+    if (this.reputation > 100) this.reputation = 100;
+    if (this.reputation < 0) this.reputation = 0;
+  } // Makes sure stats stay within 0 - 100 range
+}
